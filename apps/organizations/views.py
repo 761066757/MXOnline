43fdeede3f976e17100d3a0a479ee2fada5214bd
761066757 <761066757@qq.com>
2020-05-06 22:45:10 +0800

    登陆优化

--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from apps.organizations.models import CourseOrg,City,Teacher
 # Create your views here.
+
+class OrgView(View):
+    def get(self, request, *args, **kwargs):
+        """
+            展示
+            :param request:
+            :param args:
+            :param kwargs:
+            :return:
+        """
+        all_orgs = CourseOrg.objects.all()
+        all_nums = CourseOrg.objects.all().count()
+        all_city = City.objects.all().count()
+
+
+        # return render(request, 'org-list.html')
+        return render(request, 'org-list.html',{'all_orgs': all_orgs,})
