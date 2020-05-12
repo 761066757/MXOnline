3db3e2a0e5e528c2a1dcb621371ad58ea9b11a07
761066757 <761066757@qq.com>
2020-05-12 19:30:11 +0800

    排序

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -9,6 +9,14 @@ class CourseListView(View):
         """获取课程列表信息"""
         all_courses = Course.objects.order_by("-add_time")
 
+        # 课程排序
+        sort = request.GET.get("sort", "")
+        if sort == "students":
+            all_courses = all_courses.order_by("-students")
+        elif sort == "hot":
+            all_courses = all_courses.order_by("-click_nums")
+
+
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
@@ -16,6 +24,8 @@ class CourseListView(View):
             page = 1
         p = Paginator(all_courses, per_page=5, request=request) # 每页显示多少个
         courses = p.page(page)
+
         return render(request, "course-list.html",
                       {"all_courses": courses,
+                       "sort": sort,
                        })
